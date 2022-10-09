5af13ceeda8eb873b6526bdb77fd2252effa206a
Mika Fischer <mikafischer1003@gmail.com>
2022-10-09 18:25:51 +0200

    Create main file

---
+++ index.ts
@@ -0,0 +1,18 @@
+import { Client, ActivityType } from "discord.js";
+import chalk from "chalk";
+import config from "./config.json";
+import { readFileSync } from "fs";
+
+const secret = readFileSync("secret", { encoding: "utf-8" });
+const bot = new Client({
+    intents: "Guilds",
+    presence: {
+        activities: [{ name: "my Boss", type: ActivityType.Listening }],
+    },
+});
+
+bot.once("ready", () => {
+    console.log(chalk.blueBright + "Bot ready!");
+});
+
+bot.login(secret);
